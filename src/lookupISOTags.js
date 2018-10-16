08feec98358d72acca3fbd22adf75a439a2c1d33
Ayhan Ergul <aergul@coronaenv.com>
2018-10-16 09:50:17 -0400

    Break out lookup lists into own files

---
+++ src/lookupISOTags.js
@@ -0,0 +1,116 @@
+export default [
+  {
+    value: "farming",
+    label: "farming",
+    info:
+      "Rearing of animals and/or cultivation of plants <br/>Examples: agriculture, irrigation, aquaculture, plantations, herding, pests and diseases affecting crops and livestock"
+  },
+  {
+    value: "biota",
+    label: "biota",
+    info:
+      "Flora and/or fauna in natural environment <br/>Examples: wildlife, vegetation, biological sciences, ecology, wilderness, sealife, wetlands, habitat"
+  },
+  {
+    value: "boundaries",
+    label: "boundaries",
+    info:
+      "Legal land descriptions <br/>Examples: political and administrative boundaries"
+  },
+  {
+    value: "climatologyMeteorologyAtmosphere",
+    label: "climatology / meteorology / atmosphere",
+    info:
+      "Processes and phenomena of the atmosphere. <br/>Examples: cloud cover, weather, climate, atmospheric conditions, climate change, precipitation"
+  },
+  {
+    value: "economy",
+    label: "economy",
+    info:
+      "Economic activities, conditions and employment. <br/>Examples: production, labour, revenue, commerce, industry, tourism and ecotourism, forestry, fisheries, commercial or subsistence hunting, exploration and exploitation of resources such as minerals, oil and gas"
+  },
+  {
+    value: "elevation",
+    label: "elevation",
+    info:
+      "Height above or below sea level <br/>Examples: altitude, bathymetry, digital elevation models, slope, derived products"
+  },
+  {
+    value: "environment",
+    label: "environment",
+    info:
+      "Environmental resources, protection and conservation <br/>Examples: environmental pollution, waste storage and treatment, environmental impact assessment, monitoring environmental risk, nature reserves, landscape"
+  },
+  {
+    value: "geoscientificInformation",
+    label: "geoscientific information",
+    info:
+      "Information pertaining to earth sciences <br/>Examples: geophysical features and processes, geology, minerals, sciences dealing with the composition, structure and origin of the earth’s rocks, risks of earthquakes, volcanic activity, landslides, gravity information, soils, permafrost, hydrogeology, erosion"
+  },
+  {
+    value: "health",
+    label: "health",
+    info:
+      "Health, health services, human ecology, and safety Examples: disease and illness, factors affecting health, hygiene, substance abuse, mental and physical health, health services"
+  },
+  {
+    value: "imageryBaseMapsEarthCover",
+    label: "imagery / base maps / earth cover",
+    info:
+      "Base maps <br/>Examples: land cover, topographic maps, imagery, unclassified images, annotations"
+  },
+  {
+    value: "intelligenceMilitary",
+    label: "intelligence / military",
+    info:
+      "Military bases, structures, activities <br/>Examples: barracks, training grounds, military transportation, information collection"
+  },
+  {
+    value: "inlandWaters",
+    label: "inland waters",
+    info:
+      "Inland water features, drainage systems and their characteristics Examples: rivers and glaciers, salt lakes, water utilization plans, dams, currents, floods, water quality, hydrographic charts"
+  },
+  {
+    value: "location",
+    label: "location",
+    info:
+      "Positional information and services <br/>Examples: addresses, geodetic networks, control points, postal zones and services, place names"
+  },
+  {
+    value: "oceans",
+    label: "oceans",
+    info:
+      "Features and characteristics of salt water bodies (excluding inland waters) <br/>Examples: tides, tidal waves, coastal information, reefs"
+  },
+  {
+    value: "planningCadastre",
+    label: "planning / cadastre",
+    info:
+      "Information used for appropriate actions for future use of the land <br/>Examples: land use maps, zoning maps, cadastral surveys, land ownership"
+  },
+  {
+    value: "society",
+    label: "society",
+    info:
+      "Characteristics of society and cultures <br/>Examples: settlements, anthropology, archaeology, education, traditional beliefs, manners and customs, demographic data, recreational areas and activities, social impact assessments, crime and justice, census information"
+  },
+  {
+    value: "structure",
+    label: "structure",
+    info:
+      "Man-made construction <br/>Examples: buildings, museums, churches, factories, housing, monuments, shops, towers"
+  },
+  {
+    value: "transportation",
+    label: "transportation",
+    info:
+      "Means and aids for conveying persons and/or goods <br/>Examples: roads, airports/airstrips, shipping routes, tunnels, nautical charts, vehicle or vessel location, aeronautical charts, railways"
+  },
+  {
+    value: "utilitiesCommunication",
+    label: "utilities / communication",
+    info:
+      "Energy, water and waste systems and communications infrastructure and services <br/>Examples: hydroelectricity, geothermal, solar and nuclear sources of energy, water purification and distribution, sewage collection and disposal, electricity and gas distribution, data communication, telecommunication, radio, communication networks"
+  }
+];
---
+++ src/lookupRights.js
@@ -0,0 +1,75 @@
+export default [
+  {
+    value: "",
+    label: ""
+  },
+  {
+    value:
+      "EPA Category: Mission Sensitive, NARA Category: Critical Infrastructure",
+    label:
+      "EPA Category: Mission Sensitive, NARA Category: Critical Infrastructure"
+  },
+  {
+    value:
+      "EPA Category: Drinking Water Vulnerability Assessments, NARA Category: Critical Infrastructure-Water Assessments",
+    label:
+      "EPA Category: Drinking Water Vulnerability Assessments, NARA Category: Critical Infrastructure-Water Assessments"
+  },
+  {
+    value:
+      "EPA Category: Sensitive Drinking Water Related, NARA Category: Critical Infrastructure-Water Assessments",
+    label:
+      "EPA Category: Sensitive Drinking Water Related, NARA Category: Critical Infrastructure-Water Assessments"
+  },
+  {
+    value:
+      "EPA Category: IT Security, NARA Category: Information Systems Vulnerability Information",
+    label:
+      "EPA Category: IT Security, NARA Category: Information Systems Vulnerability Information"
+  },
+  {
+    value:
+      "EPA Category: Law Enforcement Sensitive, NARA Category: Law Enforcement",
+    label:
+      "EPA Category: Law Enforcement Sensitive, NARA Category: Law Enforcement"
+  },
+  {
+    value:
+      "EPA Category: Attorney Client Privilege, NARA Category: Legal-Privilege",
+    label:
+      "EPA Category: Attorney Client Privilege, NARA Category: Legal-Privilege"
+  },
+  {
+    value:
+      "EPA Category: Attorney Work Product, NARA Category: Legal-Privilege",
+    label: "EPA Category: Attorney Work Product, NARA Category: Legal-Privilege"
+  },
+  {
+    value:
+      "EPA Category: Deliberative Process Privilege, NARA Category: Legal-Privilege",
+    label:
+      "EPA Category: Deliberative Process Privilege, NARA Category: Legal-Privilege"
+  },
+  {
+    value:
+      "EPA Category: Personally Identifiable Information (PII), NARA Category: Privacy",
+    label:
+      "EPA Category: Personally Identifiable Information (PII), NARA Category: Privacy"
+  },
+  {
+    value: "EPA Category: Proprietary, NARA Category: Proprietary",
+    label: "EPA Category: Proprietary, NARA Category: Proprietary"
+  },
+  {
+    value:
+      "EPA Category: Confidential Business Information, NARA Category: Proprietary-Manufacturer",
+    label:
+      "EPA Category: Confidential Business Information, NARA Category: Proprietary-Manufacturer"
+  },
+  {
+    value:
+      "EPA Category: Source Selection Information, NARA Category: Proprietary-Source Selection",
+    label:
+      "EPA Category: Source Selection Information, NARA Category: Proprietary-Source Selection"
+  }
+];
