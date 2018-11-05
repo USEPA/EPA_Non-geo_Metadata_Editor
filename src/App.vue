16b3f02baa4400a45cd0349aa43ac7ae2543f550
Ayhan Ergul <aergul@coronaenv.com>
2018-11-05 17:49:34 -0500

    Prompt to avoid navigating away by mistake
    We might want to tie this into a page dirty tracker

--- src/App.vue
+++ src/App.vue
@@ -334,6 +334,24 @@ import DocumentActions from "./components/DocumentActions.vue";
 import merge from "deepmerge";
 import clean from "obj-clean";
 
+// Prompt user if they really want to navigate away from the page
+var confirmOnPageExit = function(e) {
+  // If we haven't been passed the event get the window.event
+  e = e || window.event;
+
+  var message = "Are you sure?";
+
+  // For IE6-8 and Firefox prior to version 4
+  if (e) {
+    e.returnValue = message;
+  }
+
+  // For Chrome, Safari, IE8+ and Opera 12+
+  return message;
+};
+
+window.onbeforeunload = confirmOnPageExit;
+
 export default {
   name: "app",
   components: {
