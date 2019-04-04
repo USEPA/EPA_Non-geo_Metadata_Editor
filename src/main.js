be2d48a6058315e845bc00cd52b5426552ab32a3
Ayhan Ergul <aergul@coronaenv.com>
2019-04-04 13:48:07 -0400

    Add vue-axe for accessibility audit
    #93
    and upgrade vue cli

--- src/main.js
+++ src/main.js
@@ -1,30 +1,39 @@
-import Vue from "vue";
-import App from "./App.vue";
-import UUID from "vue-uuid";
+import Vue from 'vue'
+import App from './App.vue'
+import UUID from 'vue-uuid'
 
-import "./styles/quasar.styl";
-import "quasar-framework/dist/quasar.ie.polyfills";
-import iconSet from "quasar-framework/icons/fontawesome";
-import "quasar-extras/animate";
-import "quasar-extras/roboto-font";
-import "quasar-extras/material-icons";
-import "quasar-extras/fontawesome";
-import "quasar-extras/ionicons";
-import "quasar-extras/mdi";
-import Quasar from "quasar";
+import './styles/quasar.styl'
+import 'quasar-framework/dist/quasar.ie.polyfills'
+import iconSet from 'quasar-framework/icons/fontawesome'
+import 'quasar-extras/animate'
+import 'quasar-extras/roboto-font'
+import 'quasar-extras/material-icons'
+import 'quasar-extras/fontawesome'
+import 'quasar-extras/ionicons'
+import 'quasar-extras/mdi'
+import Quasar from 'quasar'
 
 Vue.use(Quasar, {
   config: {},
   iconSet: iconSet
-});
+})
 
-import Icon from "vue-awesome/components/Icon";
-Vue.component("v-icon", Icon);
+import Icon from 'vue-awesome/components/Icon'
+Vue.component('v-icon', Icon)
 
-Vue.use(UUID);
-Vue.config.productionTip = false;
+Vue.use(UUID)
+Vue.config.productionTip = false
+
+if (process.env.NODE_ENV !== 'production') {
+  const VueAxe = require('vue-axe')
+  Vue.use(VueAxe, {
+    config: {
+      // ...
+    }
+  })
+}
 
 new Vue({
   //  store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
