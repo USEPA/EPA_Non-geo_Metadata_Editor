85764db867d84650063802a7c0af93f66d2d4607
Ayhan Ergul <aergul@coronaenv.com>
2019-08-08 01:29:40 -0400

    Editor-EDG handshake to find out user's role and change user presentation accordingly #26

--- src/components/Auth/EsriAuth.vue
+++ src/components/Auth/EsriAuth.vue
@@ -3,14 +3,14 @@
     <div v-if="loading">
       <q-btn color="blue" :icon="'fa fa-spinner fa-spin'">&nbsp;&nbsp;Checking...</q-btn>
     </div>
-    <div v-else-if="portalUser">
-      Welcome
+    <div v-else-if="edgUserData">
+      <q-tooltip>You are an {{userText}}</q-tooltip>Welcome
       <span style="font-weight: bold;">{{portalUser.fullName}}</span>
       &nbsp;&nbsp;
-      <q-btn color="blue" :icon="'fas fa-user'" @click="signOut">&nbsp;&nbsp;Sign Out</q-btn>
+      <q-btn color="blue" :icon="userIcon" @click="signOut">&nbsp;&nbsp;Sign Out</q-btn>
     </div>
     <div v-else>
-      <q-btn color="blue" :icon="'fa fa-user'" @click="signIn">&nbsp;&nbsp;Sign In</q-btn>
+      <q-btn color="blue" :icon="'far fa-user'" @click="signIn">&nbsp;&nbsp;Sign In</q-btn>
     </div>
   </div>
 </template>
@@ -26,16 +26,55 @@ export default {
     return {
       portal: null,
       portalUser: null,
-      appId: 's0brwjWwE7aFPPbF',
-      loading: true
+      loading: true,
+      edgUserData: null,
+      edgUrl: 'http://52.20.85.254:8080/metadata/'
     }
+  },
+
+  computed: {
+    userIcon: function () {
+      let icon = ''
+      if (this.portalUser)
+        if (this.edgUserData)
+          if (this.edgUserData.roles.gptAdministrator)
+            icon = 'shield'
+          else if (this.edgUserData.roles.gptPublisher)
+            icon = 'edit'
+      if (icon)
+        return 'fas fa-user-' + icon
+      else
+        return 'fa fa-user'
+    },
+
+    userText: function () {
+      let txt = ''
+      if (this.portalUser)
+        if (this.edgUserData)
+          if (this.edgUserData.roles.gptAdministrator)
+            txt = 'EDG Administrator'
+          else if (this.edgUserData.roles.gptPublisher)
+            txt = 'EDG Publisher'
+          else if (this.edgUserData.roles.gptRegisteredUser)
+            txt = 'EDG User'
+      return txt
+    },
+
   },
 
   methods: {
     populateUser (portalUser) {
       this.portalUser = portalUser
-    }
-    ,
+      if (portalUser && portalUser.credential)
+        fetch(
+          `http://52.20.85.254:8080/metadata/validateToken.jsp?u=${portalUser.credential.username}&t=${portalUser.credential.token}`
+        )
+          .then(response => response.json())
+          .then(data => {
+            console.log('EDG:', data)
+            if (data && data.user) this.edgUserData = data;
+          });
+    },
 
     signIn () {
       this.portal.signIn().then(
@@ -45,11 +84,11 @@ export default {
           // eslint-disable-next-line
           console.log("Error occurred while signing in: ", error);
         }
-
       );
     },
 
     signOut () {
+      this.loading = true
       this.portalUser = null
     }
   },
@@ -59,8 +98,10 @@ export default {
       console.log("Portal user: ", newValue);
       this.$emit('user', newValue)
       this.loading = false
-      if (!newValue)
+      if (!newValue) {
         this.portal.signOut()
+        this.edgUserData = null
+      }
     }
   },
 
@@ -70,7 +111,7 @@ export default {
     loadModules(["esri/arcgis/Portal", "esri/arcgis/OAuthInfo", "esri/IdentityManager"], options)
       .then(([portalModule, OAuthInfoModule, IdentityManagerModule]) => {
         var info = new OAuthInfoModule({
-          appId: this.appId,
+          appId: 's0brwjWwE7aFPPbF',
           portalUrl: 'https://epa.maps.arcgis.com'
         })
         this.portal = new portalModule.Portal(info.portalUrl)
